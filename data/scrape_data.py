f440ab55a759836de4822298ae8736b5c900c388
Giselle Martel <gmartel90@gmail.com>
2020-12-04 18:01:22 -0500

    scraped news

--- data/scrape_data.py
+++ data/scrape_data.py
@@ -18,7 +18,7 @@ data = {}
 data['newspapers'] = {}
 
 # Loads the JSON files with news sites
-with open('NewsPapers.json') as data_file:
+with open('news_sources.json') as data_file:
     companies = json.load(data_file)
 
 count = 1
