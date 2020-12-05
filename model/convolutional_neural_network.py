82dd88bcde62ecbf9e08a3f34389a4cd1c3c8dc8
Giselle Martel <gmartel90@gmail.com>
2020-12-05 07:00:11 -0500

    Finalize notebook code and CNN

--- model/convolutional_neural_network.py
+++ model/convolutional_neural_network.py
@@ -15,117 +15,117 @@ from torch.utils.data import Dataset, DataLoader
 import warnings
 # warnings.filterwarnings("ignore", category=sklearn.exceptions.ConvergenceWarning)
 np.set_printoptions(precision=3, suppress=True)
+torch.manual_seed(0)
 
 class CnnDataset(Dataset):
-	def __init__(self, x, y):
-		self.x = torch.Tensor(list(x.values))
-		self.y = torch.Tensor(list(y.values))
-		
-	def __len__(self):
-		return len(self.x)
-		
-	def __getitem__(self, idx):
-		return self.x[idx], self.y[idx]
+    def __init__(self, x, y):
+        self.x = torch.Tensor(list(x.values))
+        self.y = torch.Tensor(list(y.values))
+        
+    def __len__(self):
+        return len(self.x)
+        
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
 
 def construct_conv_layers(seq_len, conv_out_size, kernel_sizes, stride):
-	layers = []
-	for n in kernel_sizes:
-		layers.append(nn.Conv1d(seq_len, conv_out_size, n, stride))
-	return layers
+    layers = []
+    for n in kernel_sizes:
+        layers.append(nn.Conv1d(seq_len, conv_out_size, n, stride))
+    return layers
 
 def construct_max_pooling_layers(kernel_sizes, stride):
-	layers = []
-	for n in kernel_sizes:
-		layers.append(nn.MaxPool1d(n, stride))
-	return layers
+    layers = []
+    for n in kernel_sizes:
+        layers.append(nn.MaxPool1d(n, stride))
+    return layers
 
 def calculate_output_size(layer_out_size, kernel_size, stride):
-	return math.floor(((layer_out_size - 1 * (kernel_size - 1) - 1) /stride ) + 1)
+    return math.floor(((layer_out_size - 1 * (kernel_size - 1) - 1) /stride ) + 1)
 
 def get_fully_connected_layer_input(embedding_size, kernel_sizes, stride):
-	fully_connected_layer_input = 0
-	
-	# Determine size of conv and pool features for each layer
-	for kernel_size in kernel_sizes:
-		conv_out = calculate_output_size(embedding_size, kernel_size, stride)
-		pool_out = calculate_output_size(conv_out, kernel_size, stride)
-		fully_connected_layer_input = fully_connected_layer_input + pool_out
-	
-	return fully_connected_layer_input
+    fully_connected_layer_input = 0
+    
+    # Determine size of conv and pool features for each layer
+    for kernel_size in kernel_sizes:
+        conv_out = calculate_output_size(embedding_size, kernel_size, stride)
+        pool_out = calculate_output_size(conv_out, kernel_size, stride)
+        fully_connected_layer_input = fully_connected_layer_input + pool_out
+    
+    return fully_connected_layer_input
 
 def build_cnn_layers(params):
-	layers = {}
-	seq_len = params["seq_len"]
-	num_words = params["num_words"]
-	conv_out_size = params["conv_out_size"]
-	stride = params["stride"]
-	kernel_sizes = params["kernel_sizes"]
-	embedding_size = params["embedding_size"]
+    layers = {}
+    seq_len = params["seq_len"]
+    num_words = params["num_words"]
+    conv_out_size = params["conv_out_size"]
+    stride = params["stride"]
+    kernel_sizes = params["kernel_sizes"]
+    embedding_size = params["embedding_size"]
 
-	layers["embedding"] = nn.Embedding(num_words + 1, embedding_size, padding_idx=0)
-	
-	layers["conv_layers"] = construct_conv_layers(seq_len, conv_out_size, kernel_sizes, stride)
-	layers["pool_layers"] = construct_max_pooling_layers(kernel_sizes, stride)
-	
-	input = get_fully_connected_layer_input(embedding_size,kernel_sizes,stride) * conv_out_size
-	layers["fully_connected"] = nn.Linear(input, 1)
+    layers["embedding"] = nn.Embedding(num_words + 1, embedding_size, padding_idx=0)
+    
+    layers["conv_layers"] = construct_conv_layers(seq_len, conv_out_size, kernel_sizes, stride)
+    layers["pool_layers"] = construct_max_pooling_layers(kernel_sizes, stride)
+    
+    input = get_fully_connected_layer_input(embedding_size,kernel_sizes,stride) * conv_out_size
+    layers["fully_connected"] = nn.Linear(input, 1)
 
-	return layers
+    return layers
 
 def apply_conv_layers(conv_layers, pool_layers, x, N):
-	X_ = []
-	for i in range(N):
-		x_ = conv_layers[i](x)
-		x_ = torch.relu(x_)
-		x_ = pool_layers[i](x_)
-		X_.append(x_)
-	return X_
+    X_ = []
+    for i in range(N):
+        x_ = conv_layers[i](x)
+        x_ = torch.relu(x_)
+        x_ = pool_layers[i](x_)
+        X_.append(x_)
+    return X_
 
 class CnnModel(nn.ModuleList):	
 
-	def __init__(self, params):
-		super(CnnModel, self).__init__()
+    def __init__(self, params):
+        super(CnnModel, self).__init__()
 
-		self.dropout = nn.Dropout(0.25)
-		self.layers = build_cnn_layers(params)
+        self.dropout = nn.Dropout(0.25)
+        self.layers = build_cnn_layers(params)
 
         # parse the layers as params for the model
-		for layer in self.layers:
-			item = self.layers[layer]
-			if type(item) == list:
-				i = 1
-				name = "{}_{}".format(layer, i)
+        for layer in self.layers:
+            item = self.layers[layer]
+            if type(item) == list:
+                i = 1
+                for l in item:
+                    name = "{}_{}".format(layer, i)
+                    setattr(self, name, l)
+                    i = i + 1
+            else:
+                setattr(self,layer, item)
+        
+    def forward(self, x):
+        # Sequence of tokes is filterd through an embedding layer
+        x = torch.tensor(x).to(torch.int64)
+        x = self.layers["embedding"](x)
 
-				for l in item:
-					setattr(self, name, l)
-					i = i + 1
-			else:
-				setattr(self,layer, item)
-		
-	def forward(self, x):
-		# Sequence of tokes is filterd through an embedding layer
-		x = torch.tensor(x).to(torch.int64)
-		x = self.layers["embedding"](x)
+        assert(len(self.layers["pool_layers"]) == len(self.layers["conv_layers"]))
+        N = len(self.layers["conv_layers"])
 
-		assert(len(self.layers["pool_layers"]) == len(self.layers["conv_layers"]))
-		N = len(self.layers["conv_layers"])
+        # get output of each conv layer
+        x1, x2, x3, x4 = apply_conv_layers(self.layers["conv_layers"], self.layers["pool_layers"], x, N)
+        
+        # The output of each convolutional layer is concatenated into a unique vector
+        union = torch.cat((x1, x2, x3, x4), 2)
+        union = union.reshape(union.size(0), -1)
 
-		# get output of each conv layer
-		x1, x2, x3, x4 = apply_conv_layers(self.layers["conv_layers"], self.layers["pool_layers"], x, N)
-		
-		# The output of each convolutional layer is concatenated into a unique vector
-		union = torch.cat((x1, x2, x3, x4), 2)
-		union = union.reshape(union.size(0), -1)
-
-		# The "flattened" vector is passed through a fully connected layer
-		out = self.layers["fully_connected"](union)
-		# Dropout is applied		
-		out = self.dropout(out)
-		
-		# apply the activation function
-		out = torch.sigmoid(out)
-		
-		return out.squeeze()
+        # The "flattened" vector is passed through a fully connected layer
+        out = self.layers["fully_connected"](union)
+        # Dropout is applied		
+        out = self.dropout(out)
+        
+        # apply the activation function
+        out = torch.sigmoid(out)
+        
+        return out.squeeze()
 
 def process_batch(model, optimizer, x_batch, y_batch):
     x_batch = torch.tensor(x_batch).to(torch.int64)
@@ -193,7 +193,7 @@ def train_cnn(model, X_train, X_test, y_train, y_test, epochs, batch_size, learn
         print("Epoch: {}, loss: {:.5f}, Train accuracy: {:.5f}%, Test accuracy: {:.5f}%".format(epoch+1, training_loss.item(), train_accuracy, test_accuracy))
 
     return all_predictions, train_accuracies, test_accuracies, train_losses, test_losses
-		
+        
 def evaluation(model, loader_test):
     # Set the model in evaluation mode
     model.eval()
@@ -235,12 +235,16 @@ def test_run(X_train, X_test, y_train, y_test, use_full_dataset=False):
     print("Testing Convolutional Neural Network Classifier ...\n")
 
     # params for CNN model
+    seq_len = 224
+    if use_full_dataset: 
+        seq_len = 216
+        
     model_params = {
         # text preprocessing
-        "seq_len": 216,
+        "seq_len": seq_len,
         "num_words": 10000,
         "embedding_size": 64,
-        
+
         # size of convolution outputs
         "conv_out_size": 32,
 
@@ -251,13 +255,12 @@ def test_run(X_train, X_test, y_train, y_test, use_full_dataset=False):
         "kernel_sizes": [2,3,4,5]
     }
 
-    model = CnnModel(model_params)
-
     # training parameters
-    epochs = 18
+    epochs = 48
     batch_size = 108
     learning_rate = 0.001
 
+    model = CnnModel(model_params)
     y_pred, train_accuracies, test_accuracies, train_losses, test_losses \
         = train_cnn(
             model, 
